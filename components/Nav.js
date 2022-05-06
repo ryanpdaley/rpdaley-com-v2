8d6537d9bc44807e1c55bbfdef17ecf22fd1dd10
Ryan Daley <rpdaley@gmail.com>
2022-05-06 09:59:49 -0400

    Nav MVP

--- components/Nav.js
+++ components/Nav.js
@@ -1,25 +1,39 @@
 import Link from 'next/link';
+import { PropTypes } from 'prop-types';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import siteConfigs from '../configs/config.json';
 
 const NavStyle = styled.nav`
   background-color: red;
+  position: fixed;
+  width: 100%;
+  top: 100px;
 `;
+
+const NavBlock = ({ navData }) => {
+  const router = useRouter();
+  if (navData.isActive === true) {
+    const pageClassName =
+      router.pathname === navData.relLink ? 'navItem_active' : 'navItem';
+    return (
+      <Link href={navData.relLink}>
+        <a className={pageClassName}>{navData.name}</a>
+      </Link>
+    );
+  }
+};
 
 const Nav = () => (
   <NavStyle>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-    <Link href="/contact">
-      <a>Contact</a>
-    </Link>
-    <Link href="/ideas">
-      <a>Ideas</a>
-    </Link>
+    {siteConfigs.nav_data.map((navItem) => (
+      <NavBlock key={navItem.name} navData={navItem} />
+    ))}
   </NavStyle>
 );
+
+NavBlock.propTypes = {
+  navData: PropTypes.object,
+};
 
 export default Nav;
