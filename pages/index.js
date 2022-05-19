2e4e7963307adaea0ae230ca57195865a7e18600
Ryan Daley <rpdaley@gmail.com>
2022-05-19 15:21:55 -0400

    MVP Home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,23 @@ import Page from '../components/Page';
 export default function Home() {
   return (
     <Page>
-      <h1>This is the Home page</h1>
+      <h1>Home</h1>
+      <p>Ryan Daley (RP Daley) is a Software Developer at Shopify.</p>
+      <p>
+        He was previously a Web Application Developer at Bell Media, where he
+        created the Chromecast project.</p>
+        <p> Other previous roles include:
+        <ul>
+          <li>Automation Engineer at Apple Inc. within the CarPlay team.</li>
+          <li>Early employee at Kobo (Rakuten Kobo) within the eReader Development team.</li>
+          <li>Intern and Campus ambassador for Research In Motion.</li>
+        </ul>
+      </p>
+      <p>
+        He holds degrees from The University of British Columbia and The
+        University of Guelph.
+      </p>
+      <p>He currently resides in Toronto, Canada.</p>
     </Page>
   );
 }
