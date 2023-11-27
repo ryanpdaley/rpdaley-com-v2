ba8b7d3655bc5e8a7406156eea7a3dd1ec771084
Ryan Daley <rpdaley@gmail.com>
2023-11-27 16:46:00 -0500

    padding on bottom of Page due to Footer

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 const Page = ({ children }) => (
   <div>
     <Header />
-    <div className='pt-36 w-9/12 mx-auto'>{children}</div>
+    <div className='pt-36 pb-12 w-9/12 mx-auto '>{children}</div>
     <Footer />
   </div>
 );
