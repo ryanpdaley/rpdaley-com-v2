0ef9da20846ea39563b6b6dc696e7644956bd0db
Ryan Daley <rpdaley@gmail.com>
2023-11-30 10:11:37 -0500

    new next v: no static; now export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   trailingSlash: true,
   compiler: {
