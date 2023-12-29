37f64f647be3d5cd8fcc0a33e118dd9380522f7a
Ryan Daley <rpdaley@gmail.com>
2023-12-29 14:22:14 -0500

    Ingredients and Directions as columns for >md views

--- src/components/Recipes/components/RecipeBody/RecipeBody.tsx
+++ src/components/Recipes/components/RecipeBody/RecipeBody.tsx
@@ -7,13 +7,17 @@ const RecipeBody = ({
   checkedItems,
   setCheckedItems,
 }) => (
-  <div>
-    <RecipeIngredients
-      recipeIngredients={recipeIngredients}
-      checkedItems={checkedItems}
-      setCheckedItems={setCheckedItems}
-    />
-    <RecipeDirections recipeDirections={recipeDirections} />
+  <div className="md:flex md:flex-row md:gap-4">
+    <div className="md:basis-1/3">
+      <RecipeIngredients
+        recipeIngredients={recipeIngredients}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+      />
+    </div>
+    <div className="md:basis-2/3">
+      <RecipeDirections recipeDirections={recipeDirections} />
+    </div>
   </div>
 );
 export default RecipeBody;
