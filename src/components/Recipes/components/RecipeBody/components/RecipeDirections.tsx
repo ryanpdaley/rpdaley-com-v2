52de694500fcb05f47480e3c8be8223cbe524f94
Ryan Daley <rpdaley@gmail.com>
2023-12-30 10:56:56 -0500

    directions - pad only on left

--- src/components/Recipes/components/RecipeBody/components/RecipeDirections.tsx
+++ src/components/Recipes/components/RecipeBody/components/RecipeDirections.tsx
@@ -1,7 +1,7 @@
 const RecipeDirections = ({ recipeDirections }) => (
   <div className="block">
     <div className="text-xl pl-2">Directions:</div>
-    <ul className="list-decimal list-inside px-10">
+    <ul className="list-decimal list-inside pl-10">
       {recipeDirections.map((direction, index) => (
         <li key={index} className="-indent-8 px-8">
           {direction}
