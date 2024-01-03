46f95a927ad70309c345830d01c76a44dc3af3b4
Ryan Daley <rpdaley@gmail.com>
2024-01-02 23:25:33 -0500

    oof... didn't realize I was using dynamic routing that wasn't being statically rendered

--- src/components/Recipes/Recipe.tsx
+++ src/components/Recipes/Recipe.tsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { IoArrowBack } from 'react-icons/io5';
 import RecipeInfo from './components/RecipeInfo';
 import RecipeBody from './components/RecipeBody/RecipeBody';
 
-const RECIPE_ROOT_DIR = 'https://rpdaley.com/configs/recipes/items';
-const RECIPE_ROOT_CONFIG = 'https://rpdaley.com/configs/recipes/root.json';
-
-const RecipeView = ({ recipeData, checkedItems, setCheckedItems }) => (
-  <div className="block pb-2">
-    <div className="py-2">
-      <Link className="text-4xl inline-block align-middle" href="/recipes">
-        <div className="border-solid border-2 rounded-md mx-2">
-          <IoArrowBack />
+const RecipeView = ({ recipeData, checkedItems, setCheckedItems }) => {
+  const recipe = recipeData.data;
+  return (
+    <div className="block pb-2">
+      <div className="py-2">
+        <Link className="text-4xl inline-block align-middle" href="/recipes">
+          <div className="border-solid border-2 rounded-md mx-2">
+            <IoArrowBack />
+          </div>
+        </Link>
+        <div className="text-3xl inline-block align-middle font-oswald">
+          {recipe.info.title}
         </div>
-      </Link>
-      <div className="text-3xl inline-block align-middle font-oswald">
-        {recipeData.info.title}
+      </div>
+
+      <div className="md:border-solid md:border-2">
+        <RecipeInfo recipeInfo={recipe.info} />
+        <RecipeBody
+          recipeDirections={recipe.directions}
+          recipeIngredients={recipe.ingredients}
+          checkedItems={checkedItems}
+          setCheckedItems={setCheckedItems}
+        />
       </div>
     </div>
-
-    <div className="md:border-solid md:border-2">
-      <RecipeInfo recipeInfo={recipeData.info} />
-      <RecipeBody
-        recipeDirections={recipeData.directions}
-        recipeIngredients={recipeData.ingredients}
-        checkedItems={checkedItems}
-        setCheckedItems={setCheckedItems}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 const ErrorView = ({ id }) => (
   <div>
@@ -43,31 +43,14 @@ const ErrorView = ({ id }) => (
   </div>
 );
 
-const RecipeComponent = ({ id }) => {
-  const [recipeData, setRecipeData] = useState(null);
+const RecipeComponent = ({ data }) => {
   const [checkedItems, setCheckedItems] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetch(RECIPE_ROOT_CONFIG)
-      .then((rootData) => rootData.json())
-      .then((rootDataJson) => {
-        rootDataJson.forEach((element) => {
-          if (element.route === id[0]) {
-            fetch(`${RECIPE_ROOT_DIR}/${element.route}.json`)
-              .then((recipeFetchData) => recipeFetchData.json())
-              .then((recipeFetchJson) => {
-                setRecipeData(recipeFetchJson);
-              });
-          }
-        });
-      });
-  }, [id]);
 
   return (
     <div className="max-w-screen-xl mx-auto">
-      {recipeData && (
+      {data && (
         <RecipeView
-          recipeData={recipeData}
+          recipeData={data}
           checkedItems={checkedItems}
           setCheckedItems={setCheckedItems}
         />
---
+++ src/pages/recipes/[id].tsx
@@ -0,0 +1,28 @@
+import RecipeComponent from '../../components/Recipes/Recipe';
+
+export default function Recipe(props) {
+  return (
+    <div className="h-full">
+      <RecipeComponent data={props} />
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const result = await fetch('https://rpdaley.com/configs/recipes/root.json');
+  const routes = await result.json();
+  const paths = routes.map((element) => ({
+    params: { id: element.route.toString() },
+  }));
+  return { paths, fallback: false };
+}
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params }) {
+  const request = await fetch(
+    `https://rpdaley.com/configs/recipes/items/${params.id}.json`,
+  );
+  const recipe = await request.json();
+  return {
+    props: { data: recipe },
+  };
+}
