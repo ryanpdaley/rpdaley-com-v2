38ea0c6e2a559b79279f7e78e4320532c905b9ff
Ryan Daley <rpdaley@gmail.com>
2024-01-09 09:23:28 -0500

    functional, but not styled or optimized.

---
+++ src/components/Recipes/components/RecipePrintViews.tsx
@@ -0,0 +1,82 @@
+import RecipeBody from './RecipeBody/RecipeBody';
+import RecipeInfo from './RecipeInfo';
+
+const RecipePrintView = ({
+  recipeData,
+  checkedItems,
+  setCheckedItems,
+  ref,
+}) => {
+  console.log('RecipePrintView');
+  return (
+    <div>
+      <RecipeBody
+        recipeDirections={recipeData.directions}
+        recipeIngredients={recipeData.ingredients}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+      />
+    </div>
+  );
+};
+
+const ShoppingListPrintView = ({
+  recipeData,
+  checkedItems,
+  setCheckedItems,
+  ref,
+}) => {
+  console.log('ShoppingListPrintView');
+  return (
+    <div className="block">
+      <div className="text-2xl ml-2 mb-2 font-medium underline decoration-double font-oswald">
+        Shopping List:
+      </div>
+      <div>
+        {checkedItems.map((item, index) => (
+          <div key={index} className="ml-4">
+            <input
+              type="checkbox"
+              defaultChecked={false}
+              className="align-middle"
+            />
+            <span className="ml-2 align-middle">{item}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const RecipePrintViews = ({
+  type,
+  recipeData,
+  checkedItems,
+  setCheckedItems,
+  ref,
+}) => (
+  <div>
+    <div className="text-3xl inline-block align-middle font-oswald">
+      {recipeData.info.title}
+    </div>
+    <RecipeInfo recipeInfo={recipeData.info} />
+    {type === 'recipe' && (
+      <RecipePrintView
+        recipeData={recipeData}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+        ref={ref}
+      />
+    )}
+    {type === 'shoppingList' && (
+      <ShoppingListPrintView
+        recipeData={recipeData}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+        ref={ref}
+      />
+    )}
+  </div>
+);
+
+export default RecipePrintViews;
