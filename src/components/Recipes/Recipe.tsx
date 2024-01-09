b5496567d11857601b2d2cd1bd2568b294fe1cb6
Ryan Daley <rpdaley@gmail.com>
2024-01-09 13:51:55 -0500

    Add ability to print Recipes and Shopping lists (#124)
    * add react-to-print
    
    * functional, but not styled or optimized.
    
    * testing to see if prettier and super-linter agree on these commas :|
    
    * More styling, and hover styles

--- src/components/Recipes/Recipe.tsx
+++ src/components/Recipes/Recipe.tsx
@@ -1,40 +1,131 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { IoArrowBack } from 'react-icons/io5';
+import ReactToPrint from 'react-to-print';
+import { FaShoppingBasket } from 'react-icons/fa';
+import { MdMenuBook } from 'react-icons/md';
 import RecipeInfo from './components/RecipeInfo';
 import RecipeBody from './components/RecipeBody/RecipeBody';
+import RecipePrintViews from './components/RecipePrintViews';
+import { event as gaEvent } from '../../lib/gtag';
 
 const RECIPE_ROOT_DIR = 'https://rpdaley.com/configs/recipes/items';
 
-const RecipeView = ({ recipeData, checkedItems, setCheckedItems }) => (
-  <div className="block pb-2">
-    <div className="py-2">
-      <Link className="text-4xl inline-block align-middle" href="/recipes">
-        <div className="border-solid border-2 rounded-md mx-2">
-          <IoArrowBack />
+const RecipeView = ({
+  recipeData,
+  checkedItems,
+  setCheckedItems,
+  componentRefRecipe,
+  componentRefShoppingList,
+}) => {
+  const shoppingListIsActive = checkedItems.length > 0;
+  const shoppingListActiveElements = shoppingListIsActive
+    ? 'cursor-pointer hover:text-red-500'
+    : 'cursor-not-allowed';
+  const reactToPrintContentRecipe = useCallback(() => {
+    gaEvent({
+      action: 'Recipe Printed',
+      category: 'User Action',
+      label: 'Recipe',
+      value: recipeData.info.title,
+    });
+    return componentRefRecipe.current;
+  }, [componentRefRecipe, recipeData.info.title]);
+
+  const reactToPrintContentShoppingList = useCallback(() => {
+    gaEvent({
+      action: 'Shopping List Printed',
+      category: 'User Action',
+      label: 'Recipe',
+      value: recipeData.info.title,
+    });
+    return componentRefShoppingList.current;
+  }, [componentRefShoppingList, recipeData.info.title]);
+
+  const reactToPrintTriggerRecipe = useCallback(
+    () => (
+      <div className="block text-xl border-solid border-2 rounded-md px-2 m-1 cursor-pointer hover:text-red-500">
+        <div className="inline-block px-1 align-middle">
+          <MdMenuBook />
         </div>
-      </Link>
-      <div className="text-3xl inline-block align-middle font-oswald">
-        {recipeData.info.title}
+        <div className="inline-block align-middle font-oswald">
+          Print Recipe
+        </div>
+      </div>
+    ),
+    [],
+  );
+
+  const reactToPrintTriggerShoppingList = useCallback(
+    () => (
+      <button
+        type="button"
+        disabled={!shoppingListIsActive}
+        className={`block text-xl border-solid border-2 rounded-md px-2 m-1 ${shoppingListActiveElements}`}
+      >
+        <div className="inline-block px-1 align-middle">
+          <FaShoppingBasket />
+        </div>
+        <div className="inline-block align-middle font-oswald">
+          Print Shopping List
+        </div>
+      </button>
+    ),
+    [shoppingListActiveElements, shoppingListIsActive],
+  );
+
+  return (
+    <div className="block pb-2">
+      <div className="py-2">
+        <Link className="text-4xl inline-block align-middle" href="/recipes">
+          <div className="border-solid border-2 rounded-md mx-2">
+            <IoArrowBack />
+          </div>
+        </Link>
+        <div className="text-3xl inline-block align-middle font-oswald">
+          {recipeData.info.title}
+        </div>
+      </div>
+
+      <div className="md:border-solid md:border-2">
+        <RecipeInfo recipeInfo={recipeData.info} />
+        <RecipeBody
+          recipeDirections={recipeData.directions}
+          recipeIngredients={recipeData.ingredients}
+          checkedItems={checkedItems}
+          setCheckedItems={setCheckedItems}
+        />
+      </div>
+      <div className="block text-right">
+        <div className="inline-block">
+          <ReactToPrint
+            content={reactToPrintContentRecipe}
+            documentTitle={recipeData.info.title.replace(/\s/g, '')}
+            trigger={reactToPrintTriggerRecipe}
+          />
+        </div>
+        <div className="inline-block">
+          <ReactToPrint
+            content={reactToPrintContentShoppingList}
+            documentTitle={`${recipeData.info.title.replace(
+              /\s/g,
+              '',
+            )}-ShoppingList`}
+            trigger={reactToPrintTriggerShoppingList}
+          />
+        </div>
       </div>
     </div>
-
-    <div className="md:border-solid md:border-2">
-      <RecipeInfo recipeInfo={recipeData.info} />
-      <RecipeBody
-        recipeDirections={recipeData.directions}
-        recipeIngredients={recipeData.ingredients}
-        checkedItems={checkedItems}
-        setCheckedItems={setCheckedItems}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 const RecipeComponent = ({ data }) => {
   const { recipeId } = data;
   const [recipeData, setRecipeData] = useState(null);
   const [checkedItems, setCheckedItems] = useState<string[]>([]);
+  const componentRefRecipe = useRef<HTMLDivElement>(null);
+  const componentRefShoppingList = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     fetch(`${RECIPE_ROOT_DIR}/${recipeId}.json`)
       .then((recipeFetchData) => recipeFetchData.json())
@@ -46,11 +137,35 @@ const RecipeComponent = ({ data }) => {
   return (
     <div className="max-w-screen-xl mx-auto">
       {recipeData && (
-        <RecipeView
-          recipeData={recipeData}
-          checkedItems={checkedItems}
-          setCheckedItems={setCheckedItems}
-        />
+        <div>
+          <RecipeView
+            recipeData={recipeData}
+            checkedItems={checkedItems}
+            setCheckedItems={setCheckedItems}
+            componentRefRecipe={componentRefRecipe}
+            componentRefShoppingList={componentRefShoppingList}
+          />
+          <div className="hidden">
+            <div ref={componentRefRecipe}>
+              <RecipePrintViews
+                type="recipe"
+                recipeData={recipeData}
+                checkedItems={checkedItems}
+                setCheckedItems={setCheckedItems}
+              />
+            </div>
+          </div>
+          <div className="hidden">
+            <div ref={componentRefShoppingList}>
+              <RecipePrintViews
+                type="shoppingList"
+                recipeData={recipeData}
+                checkedItems={checkedItems}
+                setCheckedItems={setCheckedItems}
+              />
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
---
+++ src/components/Recipes/components/RecipePrintViews.tsx
@@ -0,0 +1,65 @@
+import RecipeBody from './RecipeBody/RecipeBody';
+import RecipeInfo from './RecipeInfo';
+
+const RecipePrintView = ({ recipeData, checkedItems, setCheckedItems }) => {
+  console.log('RecipePrintView');
+  return (
+    <div>
+      <RecipeBody
+        recipeDirections={recipeData.directions}
+        recipeIngredients={recipeData.ingredients}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+      />
+    </div>
+  );
+};
+
+const ShoppingListPrintView = ({ checkedItems }) => {
+  console.log('ShoppingListPrintView');
+  return (
+    <div className="block">
+      <div className="text-2xl ml-2 mb-2 font-medium underline decoration-double font-oswald">
+        Shopping List:
+      </div>
+      <div>
+        {checkedItems.map((item, index) => (
+          <div key={index} className="ml-4">
+            <input
+              type="checkbox"
+              defaultChecked={false}
+              className="align-middle"
+            />
+            <span className="ml-2 align-middle">{item}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const RecipePrintViews = ({
+  type,
+  recipeData,
+  checkedItems,
+  setCheckedItems,
+}) => (
+  <div className="p-8">
+    <div className="text-3xl inline-block align-middle font-oswald">
+      {recipeData.info.title}
+    </div>
+    <RecipeInfo recipeInfo={recipeData.info} />
+    {type === 'recipe' && (
+      <RecipePrintView
+        recipeData={recipeData}
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+      />
+    )}
+    {type === 'shoppingList' && (
+      <ShoppingListPrintView checkedItems={checkedItems} />
+    )}
+  </div>
+);
+
+export default RecipePrintViews;
