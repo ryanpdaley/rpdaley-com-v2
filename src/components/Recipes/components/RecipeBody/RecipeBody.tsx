fe5c1cb0411fd458f7df510f6ed31c7fe7379ed3
Ryan Daley <rpdaley@gmail.com>
2024-01-16 14:09:43 -0500

    RecipeBody Types

--- src/components/Recipes/components/RecipeBody/RecipeBody.tsx
+++ src/components/Recipes/components/RecipeBody/RecipeBody.tsx
@@ -1,12 +1,21 @@
+import { Dispatch, SetStateAction } from 'react';
+import { IngredientSection } from '../../types';
 import RecipeDirections from './components/RecipeDirections';
 import RecipeIngredients from './components/RecipeIngredients';
+
+type RecipeBodyType = {
+  recipeDirections: string[];
+  recipeIngredients: IngredientSection[];
+  checkedItems: string[];
+  setCheckedItems: Dispatch<SetStateAction<string[]>>;
+};
 
 const RecipeBody = ({
   recipeDirections,
   recipeIngredients,
   checkedItems,
   setCheckedItems,
-}) => (
+}: RecipeBodyType) => (
   <div className="md:flex md:flex-row md:gap-4 divide-y-2 divide-dotted md:divide-x-2 md:divide-y-0">
     <div className="md:basis-1/3 pb-2">
       <RecipeIngredients
